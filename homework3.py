063bbf45c112bc06b3814b1550f914baaffde2c4
Klukovnik <164409223+Klukovnik@users.noreply.github.com>
2024-03-25 23:38:08 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,23 @@
+print()
+
+my_string = input('Введите пожалуйста несколько произвольных слов: ')
+# игрушка ПАРК Рыбалка
+print()
+
+print('строка содержит:', len(my_string), 'символов')
+print()
+
+print(my_string.upper())
+print()
+
+print(my_string.lower())
+print()
+
+print(my_string.replace(' ', '' ))
+print()
+
+print(my_string [0])
+print()
+
+print(my_string [-1])
+print()
