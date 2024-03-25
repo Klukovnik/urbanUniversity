6fb9e824f7d8f06975cf6fa6ee430ea4b305e9ad
Klukovnik <164409223+Klukovnik@users.noreply.github.com>
2024-03-25 19:02:16 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,9 @@
+print()
+name = 'Sergei'
+age = 44
+new_age = age + 1
+is_student = new_age > age
+print('Name:', name)
+print('Age:', age)
+print('New Age:', new_age)
+print('Is student:', is_student)
