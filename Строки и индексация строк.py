20502944693cebf446394ab7e423aacc547a0aa7
Klukovnik <164409223+Klukovnik@users.noreply.github.com>
2024-03-25 09:12:03 +0300

    Add files via upload

---
+++ Строки и индексация строк.py
@@ -0,0 +1,20 @@
+print()
+kolb = 'это моя строка'
+viol = 'это новая строка'
+
+print('1.', kolb[0])
+print()
+
+print('2.',kolb[-1])
+print()
+
+print('3.',kolb[2:5])
+print()
+
+print('4.',kolb[::-1])
+print()
+
+print('5.',len(kolb), 'символов')
+print()
+
+print('6.',viol + ' ' + kolb)
