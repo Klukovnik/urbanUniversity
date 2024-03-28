676fac5b57d804870970daa8cf7b0489a231d3d0
Klukovnik <164409223+Klukovnik@users.noreply.github.com>
2024-03-28 19:50:31 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,19 @@
+print()
+my_list = ['lime', 'kiwi', 'avokado', 'grape', 'cherry', 'pear', 'peach', 'apricot']
+print('List:', my_list)
+print('First element:', my_list[0])
+print('Last element:', my_list[-1])
+print('Sublist:', my_list[2:5])
+my_list[2] = 'banana'
+print('Modified list:', my_list)
+
+print()
+my_dict = {'lime': 'лайм', 'kiwi': 'киви', 'avokado': 'авокадо', 'grape': 'виноград',
+           'cherry': 'вишня', 'pear': 'груша', 'peach': 'персик', 'apricot': 'абрикос'}
+print('Dictionary:', my_dict)
+print('Translation:', my_dict['pear'])
+my_dict['banana'] = 'банан'
+my_dict['avokado'] = 'аллигаторова груша'
+print('Modified dictionary:', my_dict)
+
+
