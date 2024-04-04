85fcbcbd92bc07c8c786cc40ece097f9a27ee611
Klukovnik <164409223+Klukovnik@users.noreply.github.com>
2024-04-04 18:29:38 +0300

    Add files via upload

---
+++ Homework7.py
@@ -0,0 +1,11 @@
+print()
+automobile = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
+for i in range(len(automobile)):
+    print('Я езжу на автомобиле', automobile[i])
+
+print()
+car_count = 0
+for i in range(len(automobile)):
+    car_count = i + 10
+    print(car_count)
+
