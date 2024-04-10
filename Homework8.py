10368f8ef10504c0223466e6f2a899c5d3e915cd
Klukovnik <164409223+Klukovnik@users.noreply.github.com>
2024-04-10 21:27:12 +0300

    Add files via upload

---
+++ Homework8.py
@@ -0,0 +1,11 @@
+def print_params(arg):
+    print(arg)
+
+print()
+print_params('apple')
+print()
+print_params('на Бога надейся, а сам неплошай')
+print()
+print_params(100 ** 2)
+print()
+print_params([1, 2, 3, 4, 150])
