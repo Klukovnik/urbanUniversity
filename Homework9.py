668b71ae562a64f5d764981c9deb29a1a6ff9d11
Klukovnik <164409223+Klukovnik@users.noreply.github.com>
2024-04-13 15:47:36 +0300

    Add files via upload

---
+++ Homework9.py
@@ -0,0 +1,11 @@
+print()
+def test_1():
+    a = 500
+    b = 710
+    print(a, b)
+def test_2(fruit_1 = 'banana', fruit_2 = 'ananas', fruit_3 = 'kiwi'):
+    print(fruit_1, fruit_2, fruit_3)
+
+test_1()
+print()
+test_2()
