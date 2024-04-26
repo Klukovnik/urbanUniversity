dc96589645337880d418beb86a6c3320f4d1ed93
Klukovnik <164409223+Klukovnik@users.noreply.github.com>
2024-04-26 12:40:24 +0300

    Add files via upload

---
+++ Homework12.py
@@ -0,0 +1,10 @@
+def print_test_1():
+    print('Hello, world!')
+
+def print_test_2():
+    print('Hello, world and space!')
+
+
+#print_test_1()
+print()
+#print_test_2()
---
+++ Homework12_main.py
@@ -0,0 +1,4 @@
+import Homework12
+Homework12.print_test_1()
+print()
+Homework12.print_test_2()
