6869c049df47701dddc86344b2e144b9dc3692fa
Klukovnik <164409223+Klukovnik@users.noreply.github.com>
2024-05-24 16:12:09 +0300

    Add files via upload

--- Homework15.py
+++ Homework15.py
@@ -1,13 +1,26 @@
 class House:
+    def __init__(self, floors = 0):
+        self.number_of_floors = floors
 
-    def __init__(self):
-        self.number_of_floors = []
-    def setNewNumberOfFloor (self, floors):
-        self.number_of_floors.append(floors)
-        print('Едем на', floors, 'этаж')
+    def set_floors(self):
+        if self.number_of_floors == 0:
+            print('остаемся здесь?')
+        else:
+            print(f'едем на {self.number_of_floors} уровень')
+
+print()
+my_house = House(15)
+my_house.set_floors()
+
+
+print()
+user_floors = int(input('на какой уровень Вам нужно? : '))
+if user_floors < 1:
+    print('Такого уровня нет в этом здании')
+else:
+    my_house = House(user_floors)
+    my_house.set_floors()
 
 print()
 my_house = House()
-my_house.setNewNumberOfFloor(floors=15)
-my_house.setNewNumberOfFloor(floors=18)
-my_house.setNewNumberOfFloor(floors=23)
+my_house.set_floors()
