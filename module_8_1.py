fb625fa432630d6e0770cabd4895da15ca06d4fc
Klukovnik <164409223+Klukovnik@users.noreply.github.com>
2024-07-31 22:54:28 +0300

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,24 @@
+print()
+def add_evrything_app(a, b):
+
+    try:
+        print(round(a + b, 3))
+
+    except:
+        print(str(a) + str(b))
+
+
+add_evrything_app(123.456, 'строка')
+print()
+add_evrything_app('яблоко', 4215)
+print()
+add_evrything_app('яблоко', 'строка')
+print()
+add_evrything_app(123.456, 7)
+
+
+
+
+
+
+
